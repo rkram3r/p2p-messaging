b2ab786ec963c06b470526522cc5b0d910bf1f05
rkram3r <skill8r@gmail.com>
2018-10-23 08:37:03 +0200

    add eslint.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "babel-polyfill";
-import PeerAdapter from "./PeerAdapter";
-import "./index.scss";
+import 'babel-polyfill';
+import PeerAdapter from './PeerAdapter';
+import './index.scss';
 
 const getElementById = id => document.getElementById(id);
 const confirmationInMilliseconds = 3000;
@@ -12,65 +12,65 @@ const setDefaultHost = () => {
 
 setDefaultHost();
 
-const listenOnMessages = p2pAdapter => {
-  p2pAdapter.listenOn("message", data => {
-    const bubble = document.createElement("div");
-    bubble.className = "speech-bubble-other";
+const listenOnMessages = (p2pAdapter) => {
+  p2pAdapter.listenOn('message', (data) => {
+    const bubble = document.createElement('div');
+    bubble.className = 'speech-bubble-other';
     bubble.innerText = data;
-    getElementById("messages").appendChild(bubble);
+    getElementById('messages').appendChild(bubble);
   });
 };
 
-const listenOnNewPeers = p2pAdapter => {
+const listenOnNewPeers = (p2pAdapter) => {
   const peers = new Map();
   const refreshRate = 2000;
-  p2pAdapter.listenOn("contactList", data => {
+  p2pAdapter.listenOn('contactList', (data) => {
     peers.set(data, new Date());
     setInterval(() => {
-      getElementById("peers").innerHTML = "";
+      getElementById('peers').innerHTML = '';
       Array.from(peers).forEach(([key, value]) => {
         if (value > new Date().setMilliseconds(-confirmationInMilliseconds)) {
-          const li = document.createElement("li");
-          li.className = "list-group-item";
+          const li = document.createElement('li');
+          li.className = 'list-group-item';
           li.appendChild(document.createTextNode(key));
-          getElementById("peers").appendChild(li);
+          getElementById('peers').appendChild(li);
         }
       });
     }, refreshRate);
   });
 };
 
-const submitPeerId = p2pAdapter => {
+const submitPeerId = (p2pAdapter) => {
   setInterval(() => {
-    p2pAdapter.broadcast("contactList", p2pAdapter.peerId);
+    p2pAdapter.broadcast('contactList', p2pAdapter.peerId);
   }, confirmationInMilliseconds);
 };
 
-const sendMessage = p2pAdapter => {
-  getElementById("send").addEventListener("click", () => {
-    const { value } = getElementById("message");
-    p2pAdapter.broadcast("message", value);
+const sendMessage = (p2pAdapter) => {
+  getElementById('send').addEventListener('click', () => {
+    const { value } = getElementById('message');
+    p2pAdapter.broadcast('message', value);
 
-    const bubble = document.createElement("div");
-    bubble.className = "speech-bubble-me";
+    const bubble = document.createElement('div');
+    bubble.className = 'speech-bubble-me';
     bubble.innerText = value;
-    getElementById("messages").appendChild(bubble);
+    getElementById('messages').appendChild(bubble);
   });
 };
-const join = p2pAdapter => {
-  getElementById("connect").addEventListener("click", () => {
-    const { value } = getElementById("connectToValue");
+const join = (p2pAdapter) => {
+  getElementById('connect').addEventListener('click', () => {
+    const { value } = getElementById('connectToValue');
     p2pAdapter.joinPeers(value);
   });
 };
 
-getElementById("connect").addEventListener("click", async () => {
-  const { value } = getElementById("connectToValue");
+getElementById('connect').addEventListener('click', async () => {
+  const { value } = getElementById('connectToValue');
   new PeerAdapter(value, [
     listenOnMessages,
     listenOnNewPeers,
     submitPeerId,
     sendMessage,
-    join
+    join,
   ]);
 });
