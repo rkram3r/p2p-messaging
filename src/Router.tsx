b70276d839c211a124fa425e884560dd54567890
rkram3r <skill8r@gmail.com>
2019-02-13 09:39:15 +0100

    simplify.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,12 @@
 import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Subscribe, Container } from "unstated";
 import App from "./pages/App";
-import AppContainer from "./container/AppContainer";
 
 export default () => (
-  <Subscribe to={[AppContainer]}>
-    {(appContainer: AppContainer) => (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/:sendTo?" component={App} />
-          <Route component={App} />
-        </Switch>
-      </BrowserRouter>
-    )}
-  </Subscribe>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/:sendTo?" component={App} />
+      <Route component={App} />
+    </Switch>
+  </BrowserRouter>
 );
